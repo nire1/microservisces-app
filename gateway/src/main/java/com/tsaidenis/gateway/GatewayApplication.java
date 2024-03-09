62e7fd0920716a6e441a1d159aede25adfce70ab
nire1 <unit003@yandex.ru>
2024-03-09 16:20:21 +0400

    eureka,gateway

--- gateway/src/main/java/com/tsaidenis/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/tsaidenis/gateway/GatewayApplication.java
@@ -3,6 +3,7 @@ package com.tsaidenis.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class GatewayApplication {
 
